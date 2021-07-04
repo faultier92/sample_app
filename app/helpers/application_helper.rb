cb2e447bfc6d58b59984d91ee53396ceb093edba
Eisuke Yoshida <ekusie.yeongjo@outlook.com>
2021-07-04 06:01:44 -0700

    Implemented helper method full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title(page_title = nil, base_title = nil)
+    base_title ||= 'Sample app'
+    page_title ? page_title + ' | ' + base_title : base_title
+  end
 end
