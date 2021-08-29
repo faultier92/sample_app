142502d3a0153f7025fc4007779cbd8f3b66ddee
Eisuke Yoshida <ekusie.yeongjo@outlook.com>
2021-08-29 14:58:59 +0900

    Implemented users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,35 @@ class UsersController < ApplicationController
   before_action :set_user, only: :show
 
   def new
+    @user = User.new
   end
 
   def show
   end
 
-  def set_user
-    @user = User.find(params[:id])
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user 
+    else
+      render 'new'
+    end
   end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name,
+                                   :email,
+                                   :admin,
+                                   :password,
+                                   :password_confirmation)
+    end
+  # End of private
 end
