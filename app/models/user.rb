bd5eb4a5d50071a8e2ad6625fab4813cc7dac0c4
Eisuke Yoshida <ekusie.yeongjo@outlook.com>
2021-09-28 19:35:52 +0900

    20210928 ch14 implemented follow and unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: 'follower_id',
                                   dependent: :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   MAX_LENGTH_OF_NAME = 50.freeze
   MAX_LENGTH_OF_EMAIL = 255.freeze
